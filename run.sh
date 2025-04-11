98720a4056ed4537e41f5bc18c3171f143472b8d
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2025-04-11 18:25:34 +0300

    update env variables

--- run.sh
+++ run.sh
@@ -17,14 +17,7 @@ then
     exit 1
 fi
 
-mkdir -p data/feeds data/files data/.dagster_home
-export DAGSTER_HOME=$(pwd)/data/.dagster_home
-export DAGSTER_FEEDS_DIR=$(pwd)/data/feeds
-export DAGSTER_FILES_DIR=$(pwd)/data/files
-
-pipenv run prefect config set PREFECT_RESULTS_PERSIST_BY_DEFAULT=true
-pipenv run prefect config set PREFECT_API_URL=http://127.0.0.1:4200/api
-pipenv run prefect config set PREFECT_LOCAL_STORAGE_PATH='./.prefect/storage'
+mkdir -p data/feeds data/files
 
 pipenv run prefect server start &
 pipenv run python -m prefect_project.datasets
