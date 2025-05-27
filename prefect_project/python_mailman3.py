cb31b99ab066dab04f74689c49ba814b491ce5d5
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2025-05-27 13:44:23 +0300

    add logs

--- prefect_project/python_mailman3.py
+++ prefect_project/python_mailman3.py
@@ -1,12 +1,16 @@
-from prefect import task
+from prefect import task, get_run_logger
 from collections import defaultdict
 
 @task
 async def to_documents(items):
+    logger = get_run_logger()
+    logger.info(f"Python mailman3: Converting {len(items)} items to documents")
     threads = defaultdict(list) 
 
     for item in items:
         threads[item["scraped_from"]["threadLink"]].append(item)
+
+    logger.info(f"Python mailman3: Found {len(threads)} threads")
 
     documents = []
     for threadLink, messages in threads.items(): 
